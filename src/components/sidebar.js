9180e8f0cd61ccaf8fea352afee05c149f1d008c
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-06-24 19:05:00 +0100

    added motion framer

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { motion } from 'framer-motion'
 
 import {
   faHouse,
