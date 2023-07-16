c229d8d873076875288949cb7b9696088c66595f
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-07-16 21:26:33 +0100

    fixed the search bug

--- src/hooks/search.jsx
+++ src/hooks/search.jsx
@@ -35,6 +35,7 @@ const Search = () => {
       <input
         type='text'
         name='search'
+        autoComplete='off'
         placeholder='Search estate agent'
         className='h-[55px] lg:w-[400px] md:w-[400px] sm:w-full sm:h-[55px] py-4 rounded-full outline-none border-2 border-color2 focus:border-hover indent-3 '
         value={searchQuery}
@@ -44,31 +45,33 @@ const Search = () => {
       />
 
       {searchQuery && (
-        <div className='w-full h-auto absolute top-20 left-0 p-5 rounded-lg shadow-2xl bg-white z-20 flex flex-col gap-5 '>
+        <div className='w-full h-[400px] absolute top-20 left-0 p-2 rounded-lg shadow-2xl bg-white z-20 flex flex-col gap-3 '>
           {searchedData?.users?.map((foundUsers) => {
             return (
               <Link
                 key={foundUsers._id}
                 href={`/users/${foundUsers._id}`}
                 onClick={() => dispatch(setSearchQuery(''))}
-                className='flex justify-start items-center gap-5 bg-white shadow-2xl p-2 rounded-lg w-full'
+                className='flex justify-start items-center gap-2 bg-white shadow-2xl p-2 rounded-lg w-full h-[60px] '
               >
                 <Image
                   src={foundUsers.avatarUrl}
                   alt={foundUsers.username}
                   width={500}
                   height={500}
-                  className='h-[70px] w-[70px] rounded-full object-cover '
+                  className='md:w-[70px] md:h-[70px] sm:h-[50px] sm:w-[50px] rounded-full object-cover border-2 border-color3 '
                 />
                 <div className='flex flex-col gap-1'>
-                  <h1 className='text-xl font-semibold'>
-                    {foundUsers.firstname} {foundUsers.lastname}
-                  </h1>
-                  <span className='text-sm text-[gray]'>
-                    @{foundUsers.username}
-                  </span>
-                  <span className='text-sm text-[gray]'>
-                    {foundUsers.address}
+                  <div className='flex flex-col'>
+                    <h1 className=' xl:text-lg md:text-lg sm:text-sm font-medium'>
+                      {foundUsers.firstname} {foundUsers.lastname}
+                    </h1>
+                    <span className='text-[12px] text-[gray]'>
+                      @{foundUsers.username}
+                    </span>
+                  </div>
+                  <span className='text-[12px] text-[gray]'>
+                    {foundUsers.city}
                   </span>
                 </div>
               </Link>
